78fe9ff0f7d56e017fd5bcc3e95f509fd5ff8463
Elias <elias.sidi@gmail.com>
2024-10-23 23:26:15 -0400

    still working on it

--- Setup/C++/BaseFile.cpp
+++ Setup/C++/BaseFile.cpp
@@ -14,25 +14,29 @@ int main() {
     cin >> num2
 
     switch (operation) {
-        case '+':
-            cout << "Result: " << num1 + num2 << endl;
-            break;
-        case '-':
-            cout << "Result: " << num1 - num2 << endl;
-            break;
-        case '*':
-            cout << "Result: " << num1 * num2 << endl;
-            break;
-        case '/':
-            if (num2 != 0)
-                cout << "Result: " << num1 / num2 << endl;
-            else
-                cout << "Error: Division by zero is not allowed." << endl;
-            break;
-        default:
-            cout << "Error: Invalid operator." << endl;
-            break;
-    }
+    case '+':
+        cout << "Result: " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << "Result: " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << "Result: " << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 != 0) {
+            cout << "Result: " << num1 / num2 << endl;
+        }
+        else {
+            cout << "Error: Division by zero is not allowed." << endl;
+        }
+        break;
+    default:
+        cout << "Error: Invalid operator." << endl;
+        break;
+}
 
     return 0;
+
+    
 }
